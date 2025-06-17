557820088e9935ce56f40d1ba3b9e036004fd6ba
AristonCatipay <aristoncatipay123@gmail.com>
2025-06-17 20:06:18 +0800

    Update the name of the the profile page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import NotFound from "./pages/NotFound";
 import Post from "./pages/Post";
 import CreatePost from "./pages/CreatePost";
 import PostDetails from "./pages/PostDetails";
-import UserPosts from "./pages/UserPosts";
+import Profile from "./pages/Profile";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
@@ -24,7 +24,7 @@ function App() {
             <Route path="/posts" element={<Post />} />
             <Route path="/posts/create" element={<CreatePost />} />
             <Route path="/posts/:id" element={<PostDetails />} />
-            <Route path="/posts/user/me" element={<UserPosts />} />
+            <Route path="/posts/user/me" element={<Profile />} />
             <Route path="*" element={<NotFound></NotFound>}></Route>
           </Routes>
         </Container>
