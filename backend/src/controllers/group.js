7a90940f69913619b17b40ab1376f3c938956a3a
AristonCatipay <aristoncatipay123@gmail.com>
2025-07-12 22:43:07 +0800

    Create the update group controller.

--- backend/src/controllers/group.js
+++ backend/src/controllers/group.js
@@ -12,7 +12,7 @@ const createGroup = async (req, res) => {
       return res.status(404).json({ error: "Post not found" });
     }
 
-    // Check if a group already exists for this post (optional)
+    // Check if a group already exists for this post
     const existingGroup = await Group.findOne({ postID });
     if (existingGroup) {
       return res
@@ -79,9 +79,41 @@ const deleteGroup = async (req, res) => {
   }
 };
 
+// PUT: Update a specific group.
+const updateGroup = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const group = await Group.findById(id);
+
+    if (!group) return res.status(404).json({ error: "No group found!" });
+
+    // Check if the authenticated user is the author
+    if (group.admin.toString() !== req.user.userId) {
+      return res
+        .status(403)
+        .json({ error: "Not authorized to update this group." });
+    }
+
+    const updatedGroup = await Group.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    ).populate("author", "username");
+
+    res.status(200).json({
+      message: "The group has been updated successfully!",
+      post: updatedGroup,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createGroup,
   getUserGroups,
   getGroup,
   deleteGroup,
+  updateGroup,
 };
