24e60c90b12d236faa661defa13a3f84c0d30b30
AristonCatipay <aristoncatipay123@gmail.com>
2026-01-07 22:18:17 +0800

    fix(post-routes): correct route order for /user/me before dynamic /:id

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -16,6 +16,9 @@ router.post("/", authMiddleware, createPost);
 // GET: http://localhost:3000/api/posts/
 router.get("/", authMiddleware, getAllPosts);
 
+// GET: http://localhost:3000/api/posts/user/me
+router.get("/user/me", authMiddleware, getUserPosts);
+
 // GET: http://localhost:3000/api/posts/:id
 router.get("/:id", authMiddleware, getPost);
 
@@ -24,8 +27,5 @@ router.delete("/:id", authMiddleware, deletePost);
 
 // PUT: http://localhost:3000/api/posts/:id
 router.put("/:id", authMiddleware, updatePost);
-
-// GET: http://localhost:3000/api/posts/user/me
-router.get("/user/me", authMiddleware, getUserPosts);
 
 module.exports = router;
